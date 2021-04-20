dda23f6be795a1b9dd778777ff270bad8617b2a1
Christopher Maigaard <xchma@energinet.dk>
2021-04-20 13:52:01 +0200

    use timer expression from app settings

--- source/business-workflow/source/Energinet.DataHub.MarketRoles.EntryPoints.Outbox/ActorMessageDispatcher.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.EntryPoints.Outbox/ActorMessageDispatcher.cs
@@ -8,12 +8,30 @@ namespace Energinet.DataHub.MarketRoles.EntryPoints.Outbox
     {
         [Function("ActorMessageDispatcher")]
         public static void Run(
-            [TimerTrigger("0 */5 * * * *")] string timer,
+            [TimerTrigger("%ACTOR_MESSAGE_DISPATCH_TRIGGER_TIMER%")] TimerInfo timer,
             FunctionContext context)
         {
             var logger = context.GetLogger("ActorMessageDispatcher");
             logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            logger.LogInformation($"Next timer schedule at: {timer}");
+            logger.LogInformation($"Next timer schedule at: {timer.ScheduleStatus.Next}");
         }
     }
+
+    #pragma warning disable SA1402 // Used only for demo usage
+    public class TimerInfo
+    {
+        public MyScheduleStatus ScheduleStatus { get; set; }
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+    #pragma warning restore SA1402
 }
