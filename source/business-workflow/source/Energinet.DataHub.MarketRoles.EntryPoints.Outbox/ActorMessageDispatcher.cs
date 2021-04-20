95644e2af7c37dd382ff8bf601b83744cd811aa6
Christopher Maigaard <xchma@energinet.dk>
2021-04-20 13:10:06 +0200

    add outbox

---
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.EntryPoints.Outbox/ActorMessageDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Energinet.DataHub.MarketRoles.EntryPoints.Outbox
+{
+    public static class ActorMessageDispatcher
+    {
+        [Function("ActorMessageDispatcher")]
+        public static void Run(
+            [TimerTrigger("0 */5 * * * *")] string timer,
+            FunctionContext context)
+        {
+            var logger = context.GetLogger("ActorMessageDispatcher");
+            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            logger.LogInformation($"Next timer schedule at: {timer}");
+        }
+    }
+}
